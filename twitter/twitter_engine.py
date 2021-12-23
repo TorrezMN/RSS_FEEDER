ab6d6bf77818d877062c026518117c1259aa9c44
TorrezMN <torrez.mn@gmail.com>
2021-12-23 11:52:23 -0300

    Added support for 'config files' and started 'twitter engine' system.

--- twitter/twitter_engine.py
+++ twitter/twitter_engine.py
@@ -1,14 +1,8 @@
-
-
 from configparser import ConfigParser
 
 config = ConfigParser()
 
-
 config.read('../config/main_config.ini')
 
-
-
-print('==='*5)
 print('SECTIONS ', config.sections())
-print('STATUS ', config['accounts']['status'])
+print('TWITTER', config['twitter'].keys())
